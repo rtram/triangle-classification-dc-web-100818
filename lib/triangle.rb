c89324167f4597b05cf162757dad4cd144ca96ce
Robin Tram <robin.tram@gmail.com>
2018-10-15 15:30:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,12 +13,14 @@ class Triangle
   end 
   
   def kind 
+    
     if (@side1 == @side2 && @side2 == @side3)
       :equilateral
     elsif (@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
       :isosceles
     elsif (@side1 != @side2 || @side2 != @side3 || @side1 != @side3)
       :scalene
+    else 
     elsif (@side1 <= 0 || @side2 <= 0 || @side3 <= 0)
       raise TriangleError
     end 
