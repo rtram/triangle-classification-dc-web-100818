a859c5b69a679751a86b037e8224b028d3d805b9
Robin Tram <robin.tram@gmail.com>
2018-10-15 15:25:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
       :isosceles
     elsif (@side1 != @side2 || @side2 != @side3 || @side1 != @side3)
       :scalene
-    elsif (@side1 == 0 || @side2 == 0 || @side3 == 0)
+    elsif (@side1 <= 0 || @side2 <= 0 || @side3 <= 0)
         raise Triangle::TriangleError
     end 
   end 
